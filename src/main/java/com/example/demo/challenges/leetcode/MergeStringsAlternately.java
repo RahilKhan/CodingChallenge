568b656f12d20ff2ef945e40a53b14a12af6cfa8
Rahil Khan <rashidali.rahil@gmail.com>
2023-07-16 16:08:25 +0530

    Added challeng solution: Merge String Alternately

--- src/main/java/com/example/demo/challenges/leetcode/MergeStringsAlternately.java
+++ src/main/java/com/example/demo/challenges/leetcode/MergeStringsAlternately.java
@@ -8,7 +8,10 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Level : Easy
  * 1768. Merge Strings Alternately
+ * https://leetcode.com/problems/merge-strings-alternately/submissions/995707038/?envType=study-plan-v2&envId=leetcode-75
+ *
  * You are given two strings word1 and word2.
  * Merge the strings by adding letters in alternating order, starting with word1.
  * If a string is longer than the other, append the additional letters onto the end of the merged string.
